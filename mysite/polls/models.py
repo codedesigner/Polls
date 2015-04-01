da359212eeb8132bbfb31884526daf260b94d7f4
Prasanna Paranjape <prasannaparanjape@gmail.com>
2015-04-01 12:50:40 +0530

    added untracked files

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 
 class Poll(models.Model):
@@ -6,3 +8,14 @@ class Poll(models.Model):
 
     def __unicode__(self):
         return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from polls.models import Poll, Choice
 
-# Create your views here.
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list' : latest_poll_list,
+        }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
